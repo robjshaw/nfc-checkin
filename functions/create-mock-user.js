c8f02b56a4d0ad74988019b15c49fa6471ab7b75
Rob Shaw <rshaw@twilio.com>
2021-10-25 14:12:08 +1100

    updated mock functionality to create or update user

--- functions/create-mock-user.js
+++ functions/create-mock-user.js
@@ -6,25 +6,61 @@ exports.handler = function(context, event, callback) {
 
     const segment = new analytics(process.env.SEGMENT, { flushAt: 1 });
 
-    segment.identify({
-        userId: event.phonenumber,
-        traits: {
-            name : faker.name.findName(),
-            email : faker.internet.email(),
-            phone : event.phonenumber,
-            state : faker.address.state(),
-            city : faker.address.city(),
-            zip : faker.address.zipCode()
+    var axios = require('axios');
+    var config = {
+        method: 'get',
+        url: 'https://profiles.segment.com/v1/spaces/' + process.env.SEGMENT_SPACEID + '/collections/users/profiles/user_id:' + event.phonenumber + '/traits',
+        headers: {
+            'Authorization': 'Basic ' + process.env.SEGMENT_PERSONAS_PROFILE_KEY
         }
-    });
+    };
 
-    segment.track({
-        userId: event.phonenumber,
-        event: 'checked-in',
-        properties: {
-            site : event.checkin
-        }
-    });
+    var result = {};
 
-    callback(null, 'done');
+    axios(config)
+        .then(function (response) {
+            // someone we know
+
+            result = response.data;
+            result.found = 1
+
+            segment.track({
+                userId: event.phonenumber,
+                event: 'checked-in',
+                properties: {
+                    site : event.checkin
+                }
+            });
+
+            callback(null, result);
+        })
+        .catch(function (error) {
+            // new user!
+
+            console.log(error);
+
+            segment.identify({
+                userId: event.phonenumber,
+                traits: {
+                    name : faker.name.findName(),
+                    email : faker.internet.email(),
+                    phone : event.phonenumber,
+                    state : faker.address.state(),
+                    city : faker.address.city(),
+                    zip : faker.address.zipCode()
+                }
+            });
+
+            segment.track({
+                userId: event.phonenumber,
+                event: 'checked-in',
+                properties: {
+                    site : event.checkin
+                }
+            });
+
+            result.found = 0;
+
+            callback(null, result);
+    });
 }
